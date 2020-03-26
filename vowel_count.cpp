ea9240a0df7d6639398668ae8d012adf9623f86e
Anubhav <hawkanubhav@gmail.com>
2020-03-26 08:00:31 +0530

    problem statement

--- vowel_count.cpp
+++ vowel_count.cpp
@@ -1,4 +1,24 @@
-/* Read input from STDIN. Print your output to STDOUT*/
+// In English, there are two types of characters, vowels and consonants. Romi is a student in school and is new to computers. He knows about vowels and consonants but does not know how to write code to get the correct output. He needs your help to learn to code and print the number of vowels, consonants and the product of them. Help Romi understand the code.
+
+// Input Format
+// The first line of input consist of number of test cases, T.
+// Next T lines consist of strings.
+
+// Constraints
+// 1<= T <=10
+// 1<= |S| <=1000
+
+// Output Format
+// For each string print the number of vowels, number of consonants and the product of them space separately.
+
+// Sample TestCase 1
+// Input
+// 2
+// abcdefgh
+// zxcvbnm
+// Output
+// 2 6 12
+// 0 7 0
 #include <iostream>
 using namespace std;
 int main()
